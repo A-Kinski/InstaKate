df75f175d7b45d652470cb77a0d3a7208a0ed4fe
A-Kinski <asidorov90@gmail.com>
2016-11-10 00:59:12 +0300

    first commit

---
+++ InstaKate/Instagram.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net.Http;
+using InstaSharp;
+using InstaSharp.Endpoints;
+using InstaSharp.Models;
+using InstaSharp.Models.Responses;
+
+
+namespace InstaKate
+{
+    class Instagram// : IDisposable
+    {
+        private const string USER_AGENT =
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) " +
+    "AppleWebKit/537.36 (KHTML, like Gecko) " +
+    "Chrome/45.0.2414.0 Safari/537.36";
+
+        private HttpClientHandler m_Handler;
+        private HttpClient m_Client;
+
+        /// <summary>
+        /// Аунтификация для InstaSharp по логину/паролю
+        /// </summary>
+        /// <param name="username">Имя пользователя</param>
+        /// <param name="password">Пароль</param>
+        /// <param name="config">конфиг InstaSharp</param>
+        /// <param name="scopes">требуемые права</param>
+        /// <returns>OAuthResponse, необходимый для дальшего использования InstaSharp</returns>
+        //public static OAuthResponse AuthByCredentials(string username, string password, InstagramConfig config, List<OAuth.Scope> scopes)
+        //{
+        //    //return AuthByCredentialsAsync(username, password, config, scopes).Result;
+        //}
+    }
+}
---
+++ InstaKate/MainPage.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Web.Http;
+using Windows.Web.Http.Filters;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace InstaKate
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private const string USER_AGENT =
+"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) " +
+"AppleWebKit/537.36 (KHTML, like Gecko) " +
+"Chrome/45.0.2414.0 Safari/537.36";
+        const string clientId = "1234567890abcdef1234567890abcdef";
+        const string redirectUri = "http://localhost/";
+        string address = $"https://api.instagram.com/oauth/authorize/?client_id={clientId}&redirect_uri={redirectUri}&response_type=token";
+        
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+
+            webView.Navigate( new Uri(address));
+
+            
+        }
+
+
+        /// <summary>
+        /// Вызывается перед отображением этой страницы во фрейме.
+        /// </summary>
+        /// <param name="e">Данные события, описывающие, каким образом была достигнута эта страница.
+        /// Этот параметр обычно используется для настройки страницы.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Подготовьте здесь страницу для отображения.
+
+            // TODO: Если приложение содержит несколько страниц, обеспечьте
+            // обработку нажатия аппаратной кнопки "Назад", выполнив регистрацию на
+            // событие Windows.Phone.UI.Input.HardwareButtons.BackPressed.
+            // Если вы используете NavigationHelper, предоставляемый некоторыми шаблонами,
+            // данное событие обрабатывается для вас.
+        }
+
+   
+
+        private void webView_FrameNavigationCompleted(WebView sender, WebViewNavigationCompletedEventArgs args)
+        {
+            var s = webView.InvokeScriptAsync("eval", new string[] { "document.cookie;" });
+        }
+
+        private void webView_NavigationCompleted(WebView sender, WebViewNavigationCompletedEventArgs args)
+        {
+            //var s = webView.InvokeScriptAsync("eval", new string[] { "document.cookie;" });
+            HttpBaseProtocolFilter baseFilter = new HttpBaseProtocolFilter();
+            //foreach (HttpCookie cookie in baseFilter.CookieManager.GetCookies(new Uri(sender.Source.ToString())))
+            //    {
+            //    if (cookie.Name.Equals("access_token"))
+            //    {
+            //        string value = cookie.Value;
+            //    }
+            //}
+            WebView webView1 = sender as WebView;
+            string html = webView1.InvokeScript("eval", new string[] { "document.documentElement.outerHTML;" });
+        }
+    }
+}
